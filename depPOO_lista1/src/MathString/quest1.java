0c09c35766ceb3d3f730e009e073d10d5b89c6f9
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2023-03-29 17:52:17 -0300

    Update quest1.java

--- depPOO_lista1/src/MathString/quest1.java
+++ depPOO_lista1/src/MathString/quest1.java
@@ -1,7 +1,16 @@
 package MathString;
 
+import java.util.Scanner;
+
 public class quest1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         double a;
+        
+        System.out.println("Digite um número para a: ");
+         a = sc.nextDouble();
+        
+        double resultado = Math.abs(a);
+        System.out.println(resultado);
     }
 }
