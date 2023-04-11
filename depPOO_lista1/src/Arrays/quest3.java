25fbbfa674e817cc19d437db6101c4a159a42c06
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2023-04-11 09:56:41 -0400

    Create quest3.java

---
+++ depPOO_lista1/src/Arrays/quest3.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class quest3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] notas = new double[10];
+        int cont = 0;
+    
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Entre com um valor: ");
+            notas[i] = sc.nextDouble();
+       }
+        
+        for(int i = 0; i < 10; i++) {
+           if(notas[i] >= 5) {
+               cont++;
+               System.out.println(cont);
+           } else {
+               System.out.println("nota baixa");
+           }
+        }
+      System.out.println(cont);
+    }
+}
