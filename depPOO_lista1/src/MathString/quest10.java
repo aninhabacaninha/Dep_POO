611aa63a2b22328ddc053339d381e8680b39af14
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2023-04-25 13:26:04 -0400

    Update quest10.java

--- depPOO_lista1/src/MathString/quest10.java
+++ depPOO_lista1/src/MathString/quest10.java
@@ -1,9 +1,16 @@
 package MathString;
 
+import java.util.Scanner;
+
 public class quest10 {
 	public static void main(String[] args) {
-		String frase = "AMOR";
-		String fraseInvertida = new StringBuilder(frase).reverse().toString();
+		String frase, fraseInvertida;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Escreva uma frase: ");
+		frase = sc.nextLine();
+	
+		fraseInvertida = new StringBuilder(frase).reverse().toString();
 		
 		System.out.println(fraseInvertida);
 	}
