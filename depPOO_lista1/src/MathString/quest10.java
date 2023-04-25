2e2ac81f7437a41393d6fb35a4406de3eecd9e1a
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2023-04-25 14:09:07 -0400

    Add files via upload

---
+++ depPOO_lista1/src/MathString/quest10.java
@@ -0,0 +1,10 @@
+package MathString;
+
+public class quest10 {
+	public static void main(String[] args) {
+		String frase = "AMOR";
+		String fraseInvertida = new StringBuilder(frase).reverse().toString();
+		
+		System.out.println(fraseInvertida);
+	}
+}
---
+++ depPOO_lista1/src/MathString/quest11.java
@@ -0,0 +1,8 @@
+package MathString;
+
+public class quest11 {
+	public static void main(String[] args) {
+		
+	}
+
+}
