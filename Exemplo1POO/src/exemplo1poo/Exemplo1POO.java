5bcedd439d505dfad33cdc1f1edf5d64040e78d5
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2023-04-26 17:11:28 -0300

    Update Exemplo1POO.java

--- Exemplo1POO/src/exemplo1poo/Exemplo1POO.java
+++ Exemplo1POO/src/exemplo1poo/Exemplo1POO.java
@@ -2,18 +2,24 @@ package exemplo1poo;
 
 public class Exemplo1POO {
     public static void main(String[] args) {
+        //ponto
         Ponto p1 = new Ponto();
-        int xP1 = p1.getX();
-        int yP1 = p1.getY();
-        
-        System.out.println("X: " + xP1);
-        System.out.println("Y: " + yP1);
+        Ponto p2 = new Ponto();
         
         p1.setPosicao(10, 5);
-         xP1 = p1.getX();
-         yP1 = p1.getY();
+        p2.setPosicao(7, 7);
+                 
+        System.out.println("X: " + p1.getX());
+        System.out.println("Y: " + p1.getY());
         
-        System.out.println("X: " + xP1);
-        System.out.println("Y: " + yP1);
+        System.out.println("X: " + p2.getX());
+        System.out.println("Y: " + p2.getY());
+        
+        //circulo
+        Círculo circulo = new Círculo();
+         circulo.x = 10;
+         circulo.y = 10;
+         circulo.raio = 5;
+        double area = circulo.CalcularArea();
     }
 }
