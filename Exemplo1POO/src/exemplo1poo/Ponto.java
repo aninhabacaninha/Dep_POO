5f2fc8650b5e39b6650bf6a0352a6f85f02f11a6
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2023-04-26 16:02:46 -0300

    Update Ponto.java

--- Exemplo1POO/src/exemplo1poo/Ponto.java
+++ Exemplo1POO/src/exemplo1poo/Ponto.java
@@ -4,16 +4,21 @@ public class Ponto {
     private int x;
     private int y;
     
+    public void setPosicao(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
     public int getX() {
         return x;
     }
     
-    public int getY() {
-        return y;
-    }
-    
     public void setX(int x) {
         this.x = x;
+    }
+    
+    public int getY() {
+        return y;
     }
     
     public void setY(int y) {
