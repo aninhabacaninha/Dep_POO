9aa68dcb3961e78f413a64631c70641a1e384b7a
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2023-04-26 15:40:04 -0300

    Add files via upload

---
+++ Exemplo1POO/src/exemplo1poo/Exemplo1POO.java
@@ -0,0 +1,12 @@
+package exemplo1poo;
+
+public class Exemplo1POO {
+    public static void main(String[] args) {
+        Ponto p1 = new Ponto();
+        int xP1 = p1.getX();
+        int yP1 = p1.getY();
+        
+        System.out.println("X: " + xP1);
+        System.out.println("Y: " + yP1);
+    }
+}
---
+++ Exemplo1POO/src/exemplo1poo/Ponto.java
@@ -0,0 +1,22 @@
+package exemplo1poo;
+
+public class Ponto {
+    private int x;
+    private int y;
+    
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+    
+    public void setX(int x) {
+        this.x = x;
+    }
+    
+    public void setY(int y) {
+        this.y = y;
+    }
+}
