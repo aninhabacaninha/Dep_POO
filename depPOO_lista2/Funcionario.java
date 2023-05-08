dc56487ff8e6bc13169ce7d53239bfa664f1cd2b
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2023-05-08 13:33:03 -0300

    Update Funcionario.java

--- depPOO_lista2/Funcionario.java
+++ depPOO_lista2/Funcionario.java
@@ -1,15 +1,50 @@
 package depPOO_lista2;
 
 public class Funcionario {
-   int codigo;
-   String nome, cargo;
-   double salario;
-    
-   public void aumentarSalarioPorTempoDeServico() {
-        salario = salario * (5/100);
-   }
-    
-   public void aumentarSalario(double taxa) {
-        salario = salario * (taxa/100);
-   }
+	
+	private int codigo;
+	private String nome, cargo;
+	private double salario;
+	
+   public double aumentarSalarioPorTempoDeServico() {
+		double salario = getSalario() * (5/100);
+		return salario;
+	}
+	
+	public double aumentarSalario(double taxa) {
+		double salario = getSalario() * (taxa/100);
+		return salario;
+	}
+   
+	public void setSalario (double salario) {
+		this.salario = salario;
+	}
+	
+	public double getSalario() {
+		return salario;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
 }
