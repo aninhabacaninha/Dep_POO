00db92bde5efd684a053e19beea7041e64075de6
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2023-05-08 14:27:59 -0300

    Add files via upload

---
+++ depPOO_lista2part2/quest1/Circulo.java
@@ -0,0 +1,13 @@
+package quest1;
+
+public class Circulo extends Ponto {
+	private double raio;
+	
+	public double getRaio() {
+		return raio;
+	}
+
+	public void setRaio(double raio) {
+		this.raio = raio;
+	}
+}
---
+++ depPOO_lista2part2/quest1/Quest1Main.java
@@ -0,0 +1,9 @@
+package quest1;
+
+public class Quest1Main {
+	public static void main(String[] args) {
+		Ponto p1 = new Ponto();
+		Circulo c1 = new Circulo();
+		
+	}
+}
