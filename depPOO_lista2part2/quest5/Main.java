bea243c73072c51f6f79f8a3dbc58636475ff087
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2023-05-11 13:59:22 -0400

    Update Main.java

--- depPOO_lista2part2/quest5/Main.java
+++ depPOO_lista2part2/quest5/Main.java
@@ -3,17 +3,36 @@ package quest5;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Cliente cliente = new Cliente();
-        
-        System.out.println("Informe seu cpf: ");
-         cliente.setCpf(sc.nextLine());
-        System.out.println("Informe seu nome: ");
-         cliente.setNome(sc.nextLine());
-        System.out.println("Digite seu email: ");
-         cliente.setEmail(sc.nextLine());
-        System.out.println("Informe seu endereço (até 5 endereços): ");
-         cliente.setValorEndereco(sc.nextLine());
-    }
+	public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        Cliente cliente = new Cliente();
+	        Endereco end = new Endereco();
+	        int numero = 0;
+	        
+	        System.out.println("Informe seu cpf: ");
+	         cliente.setCpf(sc.nextLine());
+	        System.out.println("Informe seu nome: ");
+	         cliente.setNome(sc.nextLine());
+	        System.out.println("Digite seu email: ");
+	         cliente.setEmail(sc.nextLine());
+	        
+	        System.out.println("Informe um número para adicionar endereço (no máx 5): ");
+	        for(numero = 0; numero < sc.nextInt() && numero < 5; numero++) {
+	        	sc.nextLine();
+	        	System.out.println("Informe seu Logradouro: ");
+	        	 end.setLogradouro(sc.nextLine());
+	        	System.out.println("Informe seu Complemento: ");
+	        	 end.setComplemento(sc.nextLine());
+	        	System.out.println("Informe seu Bairro: ");
+	        	 end.setBairro(sc.nextLine());
+	        	System.out.println("Informe seu Cidade: ");
+	        	 end.setCidade(sc.nextLine());
+	        	System.out.println("Informe seu CEP: ");
+	        	 end.setCep(sc.nextLine());
+	        	System.out.println("Informe seu número: ");
+	        	 end.setNumero(sc.nextInt());
+	       }
+	        
+	        cliente.adicionaEndereco(end);
+	}
 }
