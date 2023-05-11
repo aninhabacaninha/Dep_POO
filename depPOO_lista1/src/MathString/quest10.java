a225330ebc6fca866e49d6dfa37ebf341812bacf
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2023-05-11 08:00:44 -0400

    Update quest10.java

--- depPOO_lista1/src/MathString/quest10.java
+++ depPOO_lista1/src/MathString/quest10.java
@@ -1,8 +1,12 @@
 package MathString;
 
+import java.util.Scanner;
+
 public class quest10 {
 	public static void main(String[] args) {
-		String frase = "AMOR";
+		Scanner sc = new Scanner(System.in);
+		String frase;
+		
 		String fraseInvertida = new StringBuilder(frase).reverse().toString();
 		
 		System.out.println(fraseInvertida);
