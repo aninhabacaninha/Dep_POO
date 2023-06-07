84b441b1cbbffd84f3b47bdb3a23477b8f088a1e
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2023-06-07 13:24:44 -0400

    Update quest11.java

--- depPOO_lista1/src/MathString/quest11.java
+++ depPOO_lista1/src/MathString/quest11.java
@@ -1,8 +1,21 @@
 package MathString;
 
-public class quest11 {
-	public static void main(String[] args) {
-		
-	}
+import java.util.Scanner;
 
+public class quest11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	String frase, procurar;
+        int cont = 0;
+		
+	System.out.println("Digite uma frase: ");
+	 frase = sc.nextLine();
+	System.out.println("Digite uma palavra para procurar na frase: ");
+	 procurar = sc.nextLine();
+	
+         if(frase.contains(procurar) == true) {
+            cont++;
+         }
+	System.out.println(cont);
+    }
 }
