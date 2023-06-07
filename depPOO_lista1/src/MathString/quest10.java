6babd691ce569190130776219a02a965290ff4da
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2023-06-07 13:24:13 -0400

    Update quest10.java

--- depPOO_lista1/src/MathString/quest10.java
+++ depPOO_lista1/src/MathString/quest10.java
@@ -3,12 +3,15 @@ package MathString;
 import java.util.Scanner;
 
 public class quest10 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String frase;
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	String frase;
+	
+        System.out.println("Digite uma frase para ser invertida: ");
+        frase = sc.nextLine();
+	String fraseInvertida = new StringBuilder(frase).reverse().toString();
 		
-		String fraseInvertida = new StringBuilder(frase).reverse().toString();
-		
-		System.out.println(fraseInvertida);
-	}
+	System.out.println(fraseInvertida);
+    }
 }
+
