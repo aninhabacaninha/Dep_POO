e65fe4a986991914bb5cfa0a00fd05f3719d6449
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2023-10-25 15:19:42 -0400

    Add files via upload

---
+++ ExemploBancodeDados/src/exemplobancodedados/view/CadastroDependente.java
@@ -0,0 +1,14 @@
+package exemplobancodedados.view;
+
+import javax.swing.JFrame;
+
+public class CadastroDependente extends JFrame {
+	
+	public CadastroDependente() {
+		setTitle("Formulário de Cadastro de Dependente");
+        setSize(400,400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        
+	}
+}
